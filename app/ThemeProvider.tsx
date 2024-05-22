b78818e0a15cfd1011e6066d5aba87c6626537d8
rafi761 <khalif_rafi12@student.uns.ac.id>
2024-05-22 22:55:59 +0700

    fix error themeprovider

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -5,6 +5,5 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    {/* @ts-expect-error Server Component */ }
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
